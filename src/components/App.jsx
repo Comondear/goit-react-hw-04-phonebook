138570352d38dcdf8ec45befb995252f8373e75a
Comondear <elcomondear@gmail.com>
2022-12-08 15:10:09 -0700

    added-to-localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,34 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 import { Box } from './Box.styled';
+import { save, load } from './storage';
+
+const STORAGE_KEY = 'contacts';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    // contacts: [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    if (load(STORAGE_KEY)) {
+      this.setState({ contacts: load(STORAGE_KEY) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      save(STORAGE_KEY, contacts);
+    }
+  }
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
